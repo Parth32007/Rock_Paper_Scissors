f9ac407babb0e85b9544967a2ccf7980dd14291a
Parth Tambe <parthtambe327@gmail.com>
2026-06-03 14:07:41 +0530

    Added flask gameplay API

--- app.py
+++ app.py
@@ -7,14 +7,35 @@ app=Flask(__name__)
 def home():
     return "Rock Paper Scissors API Running"
 
-@app.route("/play",methods=["POST"])
+@app.route("/play", methods=["POST"])
 def play():
-    data=request.get_json()
-    player_choice=data["choice"]
-    computer=computer_choice()
-    result=winner(player_choice,computer)
 
-    return jsonify({"user": player_choice, "computer": computer, "result": result})
+    data = request.get_json()
+
+    if not data or "choice" not in data:
+        return jsonify({
+            "error": "Choice is required"
+        }), 400
+
+    user_choice = data["choice"].lower()
+
+    if user_choice not in ["rock", "paper", "scissors"]:
+        return jsonify({
+            "error": "Invalid choice"
+        }), 400
+
+    computer_choice = computer_choice()
+
+    result = winner(
+        user_choice,
+        computer_choice
+    )
+
+    return jsonify({
+        "user": user_choice,
+        "computer": computer_choice,
+        "result": result
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
